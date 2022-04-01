a7c2012fd5f8e1aac1bd4248241ac505754897f9
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-04-01 23:43:26 +0200

    Add CI

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,8 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   '*.php': ['vendor/bin/phpinsights -n --ansi --format=github-action --fix'],
-  '*.{js,jsx,ts,tsx,vue}': ['npm run lint:js --'],
-  '*.{pcss,css}': ['npm run lint:css --'],
-  '*.{json,yml,yaml,md}': ['prettier --write']
+  '*.{js,jsx,ts,tsx,vue}': ['prettier --write', 'npm run lint:js --'],
+  '*.{pcss,css}': ['prettier --write', 'npm run lint:css --'],
+  '*.{json,md}': ['prettier --write'],
+  '*.{twig}': [
+    'prettier --write',
+    'bin/console lint:twig'
+  ],
+  '*.{yml,yaml}': [
+    'prettier --write',
+    'bin/console lint:yaml'
+  ]
 }
