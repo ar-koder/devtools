d0b5f5730725db742c63b7c66b9f696f105e69f8
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-04-01 23:15:57 +0200

    Add CI

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.php': ['vendor/bin/phpinsights -n --ansi --format=github-action --fix'],
-  '*.{js,jsx,ts,tsx,vue}': ['npm run lint:js'],
-  '*.{pcss,css}': ['npm run lint:css'],
+  '*.{js,jsx,ts,tsx,vue}': ['npm run lint:js --'],
+  '*.{pcss,css}': ['npm run lint:css --'],
   '*.{json,yml,yaml,md}': ['prettier --write']
 }
