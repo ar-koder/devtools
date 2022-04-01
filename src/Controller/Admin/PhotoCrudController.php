cf271df33287e400602ac8cd354ee83755d002d8
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-04-01 23:09:49 +0200

    Add CI

---
+++ src/Controller/Admin/PhotoCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class PhotoCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Photo::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
