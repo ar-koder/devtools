b8a029f4bf92a90bc628ed3c92245cbd9ed9c3c7
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-04-05 18:31:54 +0200

    TEST

--- deploy.php
+++ deploy.php
@@ -11,7 +11,7 @@ set('ssh_multiplexing', false);
 
 set('bin/php', "/opt/php8.0/bin/php -d memory_limit=-1");
 set('bin/composer', "/opt/php8.0/bin/composer2.phar");
-set('composer_options', '--prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
+set('composer_options', '--no-progress --no-interaction --optimize-autoloader');
 // Hosts
 
 host('production')
