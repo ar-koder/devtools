549e648a21fb88df8f307fd24e8d3a421a15916d
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-04-11 22:48:19 +0200

    Optimisation admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -10,6 +10,7 @@ use App\Entity\Photo;
 use App\Entity\Post;
 use App\Entity\Todo;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Menu\CrudMenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Menu\DashboardMenuItem;
@@ -22,16 +23,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(private EntityManagerInterface $manager)
+    {}
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        return $this->render('admin/dashboard.html.twig', [
+            "counts" => [
+                "users" => $this->manager->getRepository(User::class)->count([]),
+                "posts" => $this->manager->getRepository(Post::class)->count([]),
+                "comments" => $this->manager->getRepository(Comment::class)->count([]),
+                "albums" => $this->manager->getRepository(Album::class)->count([]),
+                "photos" => $this->manager->getRepository(Photo::class)->count([]),
+                "todos" => $this->manager->getRepository(Todo::class)->count([]),
+            ]
+        ]);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('DemoApp');
+            ->setTitle('Placeholder APIs')
+            ;
     }
 
     /**
@@ -39,16 +53,27 @@ class DashboardController extends AbstractDashboardController
      */
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToUrl(label: 'Homepage', icon: 'fa fa-home', url: "/");
+
         yield MenuItem::section();
-        yield MenuItem::linkToCrud('Posts', 'fas fa-newspaper', Post::class);
-        yield MenuItem::linkToCrud('Comments', 'fas fa-reply', Comment::class);
+        yield MenuItem::linkToCrud(label: 'Posts', icon: 'fas fa-newspaper', entityFqcn: Post::class);
+        yield MenuItem::linkToCrud(label: 'Comments', icon: 'fas fa-reply', entityFqcn: Comment::class);
         yield MenuItem::section();
-        yield MenuItem::linkToCrud('Todos', 'fas fa-tasks', Todo::class);
+        yield MenuItem::linkToCrud(label: 'Todos', icon: 'fas fa-tasks', entityFqcn: Todo::class);
         yield MenuItem::section();
-        yield MenuItem::linkToCrud('Albums', 'fas fa-images', Album::class);
-        yield MenuItem::linkToCrud('Photos', 'fas fa-image', Photo::class);
+        yield MenuItem::linkToCrud(label: 'Albums', icon: 'fas fa-images', entityFqcn: Album::class);
+        yield MenuItem::linkToCrud(label: 'Photos', icon: 'fas fa-image', entityFqcn: Photo::class);
         yield MenuItem::section();
-        yield MenuItem::linkToCrud('Users', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud(label: 'Users', icon: 'fas fa-users', entityFqcn: User::class);
+
+        yield MenuItem::section(label: "Documentations", icon: "fas fa-book");
+        yield MenuItem::linkToUrl(label: "OpenAPI", icon: "fas fa-arrow-up-right-from-square", url: $this->generateUrl("api_doc"));
+        yield MenuItem::linkToUrl(label: "ReDoc", icon: "fas fa-arrow-up-right-from-square", url: $this->generateUrl("api_doc", ['ui' => 're_doc']));
+        yield MenuItem::linkToUrl(label: "GraphiQL", icon: "fas fa-arrow-up-right-from-square", url: $this->generateUrl("api_graphql_graphiql"));
+        yield MenuItem::linkToUrl(label: "GraphQL Playground", icon: "fas fa-arrow-up-right-from-square", url: $this->generateUrl("api_graphql_graphql_playground"));
+
+        yield MenuItem::section();
+        yield MenuItem::linkToUrl(label: "GitHub", icon: "fab fa-github", url: "https://github.com/arnaud-ritti/placeholder-apis");
+
     }
 }
